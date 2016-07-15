cc2bb94f40460e0733d539c133f24db7f4720dd1
Kanak Kabara <kanakkabara@hotmail.com>
2016-07-15 16:58:58 +0530

    Add files via upload

--- JavaFiles/FFMpegHLSTest.java
+++ JavaFiles/FFMpegHLSTest.java
@@ -5,27 +5,11 @@ import java.util.logging.Level;
 
 public class FFMpegHLSTest {
 	public static void main(String[] args) throws InterruptedException, IOException{
-		//timeMe();
-		List<FFMpegHLS> Q = new ArrayList<FFMpegHLS>();
-		FFMpegHLS tester = new FFMpegHLS("D:\\ffmpeg\\bin\\Community.mp4", "C:\\Apache22\\htdocs\\ffmpegTest\\test_stream\\Community_MEDIUM", FFMpegHLS.qualityLevel.MEDIUM, "D:\\ffmpeg\\bin\\ffmpeg", "D:\\ffmpeg\\bin\\ffprobe");
-		//Q.add(tester);
-		FFMpegHLS tester1 = new FFMpegHLS("D:\\ffmpeg\\bin\\Wildlife.wmv", "C:\\Apache22\\htdocs\\ffmpegTest\\test_stream\\Wildlife_MEDIUM", FFMpegHLS.qualityLevel.MEDIUM);
-		Q.add(tester1);
-//		FFMpegHLS tester2 = new FFMpegHLS("D:\\ffmpeg\\bin\\Wildlife.wmv", null, FFMpegHLS.qualityLevel.HIGH);
-//		Q.add(tester2);
-		
-		//FFMpegHLS.setLoggerLevel(Level.INFO);
-		FFMpegHLS.transcode(Q); //TRANSCODE ALL FFmpeg FILES IN PARALLEL, same as FFMpeg.transcode(Q, 0)
-		//FFMpegHLS.transcode(Q, 1); //TRANSCODE ALL FFmpeg FILES SEQUENTIALLY
-		//FFMpegHLS.transcode(Q, 2); //TRANSCODE ALL FFmpeg FILES, WITH 2 RUNNING IN PARALLEL 
-	}
-	public static void timeMe(){
-		final long start = System.currentTimeMillis();	
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-		    public void run() {
-				long end = System.currentTimeMillis();
-				System.out.println("Runtime: "+(end-start)+" milliseconds");
-		    }
-		});
+		FFMpegHLS tester = new FFMpegHLS("D:\\ffmpeg\\bin\\1min.mp4", "D:\\ffmpeg\\bin\\outputs", FFMpegHLS.qualityLevel.LOW, "D:\\ffmpeg\\bin\\ffmpeg", "D:\\ffmpeg\\bin\\ffprobe");
+		long startTime = System.nanoTime();
+		Process proc = tester.transcode();
+		tester.trackProgress(proc);
+		long endTime = System.nanoTime();
+		System.out.println("Total time: " + (endTime - startTime)/1000000 + "\n");
 	}
 }
--- JavaFiles/FFMpegTest.java
+++ JavaFiles/FFMpegTest.java
@@ -5,28 +5,18 @@ import java.util.logging.Level;
 
 public class FFMpegTest {
 	public static void main(String[] args) throws InterruptedException, IOException{
-		//timeMe();
-		List<FFMpeg> Q = new ArrayList<FFMpeg>();
+//		List<FFMpeg> Q = new ArrayList<FFMpeg>();
 		FFMpeg tester = new FFMpeg("D:\\ffmpeg\\bin\\citylifestyle.mp4", null, FFMpeg.qualityLevel.MEDIUM, "D:\\ffmpeg\\bin\\ffmpeg", "D:\\ffmpeg\\bin\\ffprobe");
-		Q.add(tester);
+		tester.trackProgress(tester.transcode());
+//		Q.add(tester);
 //		FFMpeg tester1 = new FFMpeg("D:\\ffmpeg\\bin\\Wildlife.wmv", null, FFMpeg.qualityLevel.MEDIUM);
 //		Q.add(tester1);
 //		FFMpeg tester2 = new FFMpeg("D:\\ffmpeg\\bin\\Wildlife.wmv", null, FFMpeg.qualityLevel.HIGH);
 //		Q.add(tester2);
 
 //		FFMpeg.setLoggerLevel(Level.INFO);
-		FFMpeg.transcode(Q); //TRANSCODE ALL FFmpeg FILES IN PARALLEL, same as FFMpeg.transcode(Q, 0)
+//		FFMpeg.transcode(Q); //TRANSCODE ALL FFmpeg FILES IN PARALLEL, same as FFMpeg.transcode(Q, 0)
 //		FFMpeg.transcode(Q, 1); //TRANSCODE ALL FFmpeg FILES SEQUENTIALLY
 //		FFMpeg.transcode(Q, 2); //TRANSCODE ALL FFmpeg FILES, WITH 2 RUNNING IN PARALLEL 
-	}
-	
-	public static void timeMe(){
-		final long start = System.currentTimeMillis();	
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-		    public void run() {
-				long end = System.currentTimeMillis();
-				System.out.println("Runtime: "+(end-start)+" milliseconds");
-		    }
-		});
 	}
 }
